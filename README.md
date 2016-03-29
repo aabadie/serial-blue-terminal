8bd37ea47068de561decb9e5f24c35a9f1e67ea3
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-03-29 10:45:52 +0200

    polishing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Serial Blue Terminal
 
 This application allows to connect to a serial port or to a bluetooth device via the rfcomm service.
+
+For the moment, only the serial interface is available. The bluetooth RFCOMM will come soon.
--- cmake/CPackDebConfig.cmake
+++ cmake/CPackDebConfig.cmake
@@ -1,6 +1,7 @@
 set(CPACK_GENERATOR ${CPACK_GENERATOR};DEB)
 set(CPACK_DEB "ON")
-set(CPACK_DEB_PACKAGE_DESCRIPTION "Serial and RFCOMM Bluetooth communication application")
+
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Serial and RFCOMM Bluetooth communication application")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5widgets5 (>=5.4), libqt5bluetooth5 (>=5.4), libqt5serialport5 (>=5.4)")
 set(CPACK_DEBIAN_PACKAGE_SECTION "Communication")
 set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "gtkterm")
